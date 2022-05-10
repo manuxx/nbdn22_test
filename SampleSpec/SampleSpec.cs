c557b03518179e43f6c99f141a0ba6d73e20ace9
manuxx <manusxyz@gmail.com>
2022-05-10 22:31:45 +0200

    initial commit

---
+++ SampleSpec/SampleSpec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace SampleSpec
+{
+    public class when_increasing_int 
+    {
+//      Establish context = () => {  sut = 1; };
+
+//      Because of = () => { sut = sut + 1; };
+
+        private It should_be_equal_to_two = () =>
+        {
+                    (1 + 1).ShouldEqual(2);
+
+        };
+//      It should_be_equal_to_two1 = () => { };
+
+        private static int sut;
+    }
+}
